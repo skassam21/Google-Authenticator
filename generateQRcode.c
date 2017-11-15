2f8c60944ba657f0a6cb024ba42a6a297ea3b0a9
Shums Kassam <skassam21@gmail.com>
2017-11-14 21:03:47 -0500

    validate htop

--- generateQRcode.c
+++ generateQRcode.c
@@ -5,6 +5,17 @@
 
 #include "lib/encoding.h"
 
+void convertStrToHex(char* inputStr, uint8_t*outputStr) {
+	int i;
+	int j;
+	char subStr[2];
+	for (i = 0, j = 0; i < strlen(inputStr); i+=2, j++) {
+		subStr[0] = inputStr[i];
+		subStr[1] = inputStr[i+1];
+		outputStr[j] = strtol(subStr, NULL, 16);
+	}
+}
+
 int
 main(int argc, char * argv[])
 {
@@ -24,8 +35,29 @@ main(int argc, char * argv[])
 
 	// Create an otpauth:// URI and display a QR code that's compatible
 	// with Google Authenticator
+	// Convert secret_hex from string to hex representation
+	uint8_t buf[10];
+	convertStrToHex(secret_hex, buf);
 
-	displayQRcode("otpauth://testing");
+	// Base32 encode the secret
+	uint8_t secretHexEncoded[16];
+	base32_encode((const uint8_t *) buf, 10, secretHexEncoded, 16);
+
+	// URL encode account Name and issuer
+	const char* accountNameEncoded = urlEncode(accountName);
+	const char* issuerEncoded = urlEncode(issuer);
+
+	// Generate the HOTP URI
+	char *hotpURI = malloc(14 + strlen(accountNameEncoded) + 8 + strlen(issuerEncoded) + 8 + 16 + 10);
+	sprintf(hotpURI, "otpauth://hotp/%s?issuer=%s&secret=%s&counter=1", accountNameEncoded, issuerEncoded, secretHexEncoded);
+
+	displayQRcode(hotpURI);
+
+	// Generate the TOTP URL
+	char *totpURI = malloc(14 + strlen(accountNameEncoded) + 8 + strlen(issuerEncoded) + 8 + 16 + 10);
+	sprintf(totpURI, "otpauth://totp/%s?issuer=%s&secret=%s&period=30", accountNameEncoded, issuerEncoded, secretHexEncoded);
+
+	displayQRcode(totpURI);
 
 	return (0);
 }
--- validateQRcode.c
+++ validateQRcode.c
@@ -1,12 +1,116 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
 
 #include "lib/sha1.h"
+
+void convertStrToHex(char* inputStr, uint8_t*outputStr) {
+	int i;
+	int j;
+	char subStr[2];
+	for (i = 0, j = 0; i < strlen(inputStr); i+=2, j++) {
+		subStr[0] = inputStr[i];
+		subStr[1] = inputStr[i+1];
+		outputStr[j] = strtol(subStr, NULL, 16);
+	}
+}
+
+void hmac(const uint8_t *secret, int secretLength,
+          const uint8_t *message, int messageLength,
+          uint8_t *result, int resultLength) {
+	SHA1_INFO ctx;
+
+	uint8_t tmp_secret[64];
+	for (int i = 0; i < secretLength; ++i) {
+	tmp_secret[i] = secret[i] ^ 0x36;
+	}
+
+	memset(tmp_secret + secretLength, 0x36, 64 - secretLength);
+
+	sha1_init(&ctx);
+	sha1_update(&ctx, tmp_secret, 64);
+	sha1_update(&ctx, message, messageLength);
+	uint8_t sha[SHA1_DIGEST_LENGTH];
+	sha1_final(&ctx, sha);
+
+	for (int i = 0; i < secretLength; ++i) {
+	tmp_secret[i] = secret[i] ^ 0x5C;
+	}
+	memset(tmp_secret + secretLength, 0x5C, 64 - secretLength);
+
+	sha1_init(&ctx);
+	sha1_update(&ctx, tmp_secret, 64);
+	sha1_update(&ctx, sha, SHA1_DIGEST_LENGTH);
+	sha1_final(&ctx, sha);
+
+	// Copy result to output buffer and truncate or pad as necessary
+	memset(result, 0, resultLength);
+	memcpy(result, sha, resultLength);
+}
 
 static int
 validateHOTP(char * secret_hex, char * HOTP_string)
 {
+
+	// Convert secret_hex from string to binary representation
+	uint8_t data[10];
+	convertStrToHex(secret_hex, data);
+
+	uint8_t hmac_result[SHA1_DIGEST_LENGTH];
+
+	uint8_t counter[8];
+
+	int i;
+	int j;
+	for (i = 1; i <= 5; i++) {
+
+		// Set the counter to the value if i
+		for (j = 0; j < 8; j++) {
+			counter[j] = 0x00;
+		}
+		counter[7] = i; // highest bit is hashed first
+
+		printf ("Counter: ");
+		for (j = 0; j < 8; j++)
+		  printf ("%02x ", counter[j] & 0xFF);
+		printf ("\n");
+
+		printf ("Hex: ");
+		for (j = 0; j < 10; j++)
+		  printf ("%02x ", data[j] & 0xFF);
+		printf ("\n");
+
+		hmac(data, 10, counter, 8, hmac_result, SHA1_DIGEST_LENGTH);
+		printf ("HMAC: ");
+		for (j = 0; j < 20; j++)
+		  printf ("%02x ", hmac_result[j] & 0xFF);
+		printf ("\n");
+
+		// offset is the byte that represents the 4 lower bits of last part
+		uint8_t offset = hmac_result[19] & 0x0f;
+		printf ("offset: %02x", offset & 0xFF);
+		printf ("\n");
+
+		long S;
+		S = (((hmac_result[offset] & 0x7f) << 24)
+		 | ((hmac_result[offset + 1] & 0xff) << 16)
+		 | ((hmac_result[offset + 2] & 0xff) << 8) | ((hmac_result[offset + 3] & 0xff)));
+
+		printf ("value: %ld\n", S);
+
+		long result = S % (long) pow(10.0, 6.0);
+
+		printf ("result: %ld\n", result);
+		printf ("HOTP: %ld\n", strtol(HOTP_string, NULL, 10));
+
+		// Compare HOTP_string with result
+		if (strtol(HOTP_string, NULL, 10) == result) {
+			return(1);
+		}
+
+	}
 	return (0);
 }
 
